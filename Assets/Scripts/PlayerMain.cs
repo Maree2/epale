34c672570f0d900f058dcc578c2b45c8213e3548
Jorge Palacios <pctroll@gmail.com>
2014-08-23 12:39:18 -0430

    PlayerMain movement on stage

--- Assets/Scripts/PlayerMain.cs
+++ Assets/Scripts/PlayerMain.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMain : MonoBehaviour {
+public class PlayerMain : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float speed;
+
+    void Update()
+    {
+        float velX = Input.GetAxis("Horizontal");
+        float velY = Input.GetAxis("Vertical");
+        Vector3 vec = new Vector3(velX, 0.0f, velY);
+        vec = Quaternion.AngleAxis(45, Vector3.up) * vec;
+        Vector3 oldPos = gameObject.transform.position;
+        gameObject.transform.Translate(vec * speed * Time.deltaTime, Space.World);
+        Vector3 vel = gameObject.transform.position - oldPos;
+        if (vel.sqrMagnitude > 0.0f)
+        {
+            vel = vel + gameObject.transform.position;
+            gameObject.transform.LookAt(vel);
+        }
+    }
+
 }
